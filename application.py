1e1872b3d5dd7829027fac791fcafd6c2bea7e3c
Aaron-Marker <8ordeaux@gmail.com>
2022-08-26 04:24:35 -0400

    Pushing to AWS

--- application.py
+++ application.py
@@ -1,27 +1,18 @@
 from flask import Flask,render_template,request,redirect, url_for
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
 import tensorflow as tf
+#Author: Aaron Marker
 
 application = Flask(__name__)
 
+#Load Model
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-#from transformers import AutoModel
-
-#model = AutoModel.from_pretrained("AaronMarker/my-awesome-model")
-model = TFAutoModelForSequenceClassification.from_pretrained("AaronMarker/my-awesome-model", num_labels=6)
-#model.load_weights("model")
+model = TFAutoModelForSequenceClassification.from_pretrained("AaronMarker/emotionClassifier", num_labels=9)
 classifier = pipeline("text-classification", model = model, tokenizer = tokenizer)
-f = open("demofile.txt", "r")
-test = f.read()
 
-#@application.before_first_request
-#def load_model():
       
-
 @application.route('/')
 def home():
-    #if request.method == 'POST':
-    #    sentence = request.form["home"]
     return render_template('site.html')
 
 @application.route('/results/', methods = ['POST', 'GET'])
@@ -30,7 +21,15 @@ def results():
         return redirect(url_for('home'))
     if request.method == 'POST':
         sentence = request.form["sentence"]
-        emotions = {'LABEL_0':'Sadness', 'LABEL_1':'Joy', 'LABEL_2':'Love', 'LABEL_3':'Anger', 'LABEL_4':'Fear', 'LABEL_5':'Surprise'}
+        emotions = {'LABEL_0':'Joy', 
+                    'LABEL_1':'Desire', 
+                    'LABEL_2':'Admiration', 
+                    'LABEL_3':'Approval', 
+                    'LABEL_4':'Curiosity', 
+                    'LABEL_5':'Fear', 
+                    'LABEL_6':'Sadness', 
+                    'LABEL_7':'Anger', 
+                    'LABEL_8':'Neutral'}
         prediction = emotions[classifier(sentence)[0]["label"]]
-        form_data = [sentence, prediction, test]
+        form_data = [sentence, prediction]
         return render_template('data.html',form_data = form_data)
